1f22498b578aaa94cd9362a2b0bd7d97972a79c9
Trevor <trevku155@gmail.com>
2018-07-29 14:21:56 -0400

    Add 10 day forecast data

--- app/components/WeatherData.js
+++ app/components/WeatherData.js
@@ -2,7 +2,23 @@ import React from 'react';
 
 export class WeatherData extends React.Component {
     render() {
+        // for debugging
+        console.log("Data sent into WeatherData component:");
         console.log(this.props.data);
+
+        let forecasts = this.props.data.forecast;
+        let forecast_data = forecasts.map(function(forecast, i) {
+            return (
+                <li key={i}>
+                    <p>Date: {forecast.date}</p>
+                    <p>Day: {forecast.day}</p>
+                    <p>High: {forecast.high}</p>
+                    <p>Low: {forecast.low}</p>
+                    <p>{forecast.text}</p>
+                </li>
+            );
+        });
+
         return (
             <div id="weather_data">
                 <p>Sunrise: {this.props.data.sunrise}</p>
@@ -11,8 +27,13 @@ export class WeatherData extends React.Component {
 
                 <p>Current Temperature: {this.props.data.current_condition.temp}</p>
                 <p>{this.props.data.current_condition.text}</p>
+
+                <h3>10 Day Forecast</h3>
+                <ul>
+                    {forecast_data}
+                </ul>
+
             </div>
         );
-
     }
 }
