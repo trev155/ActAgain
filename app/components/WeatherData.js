950342d1633a643d5cede7020f72ba3b09fe1629
Trevor <trevku155@gmail.com>
2018-07-30 19:45:33 -0400

    More styling. Everything looks good now, I have nice looking tiles and good visual aesthethics.

--- app/components/WeatherData.js
+++ app/components/WeatherData.js
@@ -9,45 +9,45 @@ export class WeatherData extends React.Component {
         let forecasts = this.props.data.forecast;
         let forecast_data = forecasts.map(function(forecast, i) {
             return (
-                <div className="forecastBlock" key={i}>
-                    <p className="stretch">{forecast.day} {forecast.date}</p>
+                <div className="blockSection" key={i}>
+                    <p className="day">{forecast.day} {forecast.date}</p>
                     <img src="/app/images/placeholder.png" width="50" height="50"/>
-                    <p className="stretch">{forecast.low}&deg;C - {forecast.high}&deg;C</p>
-                    <p>{forecast.text}</p>
+                    <p className="highlow">{forecast.low}&deg;C - {forecast.high}&deg;C</p>
+                    <p className="description">{forecast.text}</p>
                 </div>
             );
         });
 
         return (
             <div className="WeatherData">
-                <div className="CurrentWeather">
+                <div className="currentWeather">
                     <h3 className="subheader">Current Conditions for: {this.props.data.location.city}, {this.props.data.location.region}, {this.props.data.location.country}</h3>
                     <p className="dateDescriptor">Data retrieved at: (placeholder)</p>
 
                     <div className="blockSection">
-                        <p>Sunrise</p>
+                        <p class="itemName">Sunrise</p>
                         <img src="/app/images/sunrise.png" width="50" height="50"/>
-                        <p>{this.props.data.sunrise}</p>
+                        <p class="itemValue">{this.props.data.sunrise}</p>
                     </div>
                     <div className="blockSection">
-                        <p>Sunset</p>
+                        <p class="itemName">Sunset</p>
                         <img src="/app/images/sunset.png" width="50" height="50"/>
-                        <p>{this.props.data.sunset}</p>
+                        <p class="itemValue">{this.props.data.sunset}</p>
                     </div>
                     <div className="blockSection">
-                        <p>Humidity</p>
+                        <p class="itemName">Humidity</p>
                         <img src="/app/images/humidity.png" width="50" height="50"/>
-                        <p>{this.props.data.humidity}</p>
+                        <p class="itemValue">{this.props.data.humidity}</p>
                     </div>
                     <div className="blockSection">
-                        <p>Current Temperature</p>
+                        <p class="itemName">Current Temperature</p>
                         <img src="/app/images/temperature.png" width="50" height="50"/>
-                        <p>{this.props.data.current_condition.temp}&deg;C</p>
+                        <p class="itemValue">{this.props.data.current_condition.temp}&deg;C</p>
                     </div>
                     <div className="blockSection">
-                        <p>Conditions</p>
+                        <p class="itemName">Conditions</p>
                         <img src="/app/images/placeholder.png" width="50" height="50"/>
-                        <p>{this.props.data.current_condition.text}</p>
+                        <p class="itemValue">{this.props.data.current_condition.text}</p>
                     </div>
                 </div>
 
